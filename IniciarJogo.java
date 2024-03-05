a0025e07cda8706c8cadcbbd05a276b246168e67
Anthony Gabriel <anthonygosdev@gmail.com>
2024-03-04 21:49:00 -0300

    Add files via upload

---
+++ IniciarJogo.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class IniciarJogo extends JFrame {
+
+    public static void main(String[] args) {
+        new IniciarJogo();
+    }
+    
+    IniciarJogo() {
+        add(new TelaJogo());
+        setTitle("Snake Game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+}
